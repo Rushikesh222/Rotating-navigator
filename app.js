26c20d6ab7456c0a670206de9ff7f06e9d65841e
Rushikesh Shirsat <shirsatrushikesh6@gmail.com>
2021-10-29 19:56:41 +0530

    complete

---
+++ app.js
@@ -0,0 +1,5 @@
+open = document.getElementById("open");
+close = document.getElementById("close");
+container = document.querySelector(".container");
+open.addEventListener("click", () => container.classList.add("show-nav"));
+close.addEventListener("click", () => container.classList.remove("show-nav"));
